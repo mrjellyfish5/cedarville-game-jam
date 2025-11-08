4b6b03ffef9043fbf9932bc0098b4fd7bbc214a3
Josiah Main <josiahmain@cedarville.edu>
2025-11-08 13:10:25 -0500

    update

--- scripts/Lighting/lighting.gd
+++ scripts/Lighting/lighting.gd
@@ -1,8 +1,16 @@
 extends CharacterBody2D
 
+@export var health = 1
+
 func lamp_fall(direction):
-	$crash.play()
-	$CollisionShape2D.queue_free()
-	$anim.play("fall_" + direction)
-	await $anim.animation_finished
-	queue_free()
+	health -= 1
+	if health <= 0:
+		$crash.play()
+		$CollisionShape2D.queue_free()
+		$anim.play("fall_" + direction)
+		await $anim.animation_finished
+		queue_free()
+	else:
+		$hit.play()
+		health -= 1
+		$anim.play("swing")

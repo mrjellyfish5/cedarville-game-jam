a095ba612acd4047f9780afaf43995496b387288
mrjellyfish5 <codytibbetts@cedarville.edu>
2025-11-08 10:09:06 -0500

    added movement

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -1,1 +1,25 @@
 extends CharacterBody2D
+
+const PLAYABLE_SPEED: float = 200
+const JUMP_VEL: float = -1000
+const GRAVITY: float = 750
+
+var grav_vel: float = 0
+var jumps_remaining: int = 3
+
+func _process(delta):
+	if Input.is_action_pressed("Move Right"):
+		position.x += PLAYABLE_SPEED * delta
+		$Sprite.flip_h = false
+	if Input.is_action_pressed("Move Left"):
+		position.x -= PLAYABLE_SPEED * delta
+		$Sprite.flip_h = true
+	position.y += grav_vel * delta
+	grav_vel += GRAVITY * delta
+	if Input.is_action_just_pressed("Jump") and jumps_remaining > 0:
+		$anim.play("idle")
+		jump()
+		jumps_remaining -= 1
+
+func jump() -> void:
+	grav_vel = JUMP_VEL

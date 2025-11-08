5563414dae03a7094badce08e2ad2ddfec788b3b
Josiah Main <josiahmain@cedarville.edu>
2025-11-08 10:24:35 -0500

    update

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -7,13 +7,15 @@ const GRAVITY: float = 750
 var grav_vel: float = 0
 var jumps_remaining: int = 3
 
-func _process(delta):
+func _physics_process(delta):
 	if Input.is_action_pressed("Move Right"):
 		position.x += PLAYABLE_SPEED * delta
-		$Sprite.flip_h = false
+		$idle.flip_h = false
+		$attack.flip_h = false
 	if Input.is_action_pressed("Move Left"):
 		position.x -= PLAYABLE_SPEED * delta
-		$Sprite.flip_h = true
+		$idle.flip_h = true
+		$attack.flip_h = true
 	position.y += grav_vel * delta
 	grav_vel += GRAVITY * delta
 	if Input.is_action_just_pressed("Jump") and jumps_remaining > 0:
@@ -23,3 +25,9 @@ func _process(delta):
 
 func jump() -> void:
 	grav_vel = JUMP_VEL
+
+func attack():
+	set_physics_process(false)
+	$anim.play("attack")
+	await $anim.animation_finished
+	set_physics_process(true)
